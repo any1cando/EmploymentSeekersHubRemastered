0064dcbd86afce3cdb523893c86ce9262193a8be
any1cando <maksimchik2003@bk.ru>
2024-09-13 19:07:42 +0300

    Сделал отображение первоначального фрагмента для авторизации, начать делать логику с транзакциями и кнопками "Sign up!", "Log in".

--- app/src/main/java/com/example/employmentseekershubremastered/MainActivity.kt
+++ app/src/main/java/com/example/employmentseekershubremastered/MainActivity.kt
@@ -2,14 +2,41 @@ package com.example.employmentseekershubremastered
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.example.employmentseekershubremastered.databinding.ActivityMainBinding
+import com.example.employmentseekershubremastered.databinding.ActivityMainWithBottomNavigationBinding
+import com.example.employmentseekershubremastered.fragments.entry.point.AuthorizationFragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var bindingWithBottomNavigation: ActivityMainWithBottomNavigationBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Инфлейтим binding для макета с реализацией фрагментов через FragmentManager/FragmentTransaction
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Инфлейтим binding для макета с навигацией фрагментов через нижнее меню (с Navigation Jetpack)
+        bindingWithBottomNavigation = ActivityMainWithBottomNavigationBinding.inflate(layoutInflater)
+
+        // Открываем фрагмент авторизации по умолчанию
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.authorizationAndRegistrationFragmentContainer, AuthorizationFragment())
+                .commit()
+        }
+
+    }
+
+
+    // Метод, с помощью которого можно переходить между фрагментами
+    fun navigateToFragment(fragment: Fragment, addToBackStack: Boolean = true) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.authorizationAndRegistrationFragmentContainer, fragment)
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
     }
 }
