2e5a642416b660d923866906b6e750be851ee1ff
any1cando <maksimchik2003@bk.ru>
2024-09-16 03:10:01 +0300

    Убрал все лишние "TODO" в фрагментах. | Посмотреть гайд на реализацию Retrofit. В RegistrationFragment сделать "TODO". Сделать нормальные иконки для нижнего меню навигации.

--- app/src/main/java/com/example/employmentseekershubremastered/fragments/main/ThirdFragment.kt
+++ app/src/main/java/com/example/employmentseekershubremastered/fragments/main/ThirdFragment.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.employmentseekershubremastered.R
 
-// TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
@@ -18,7 +17,7 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class ThirdFragment : Fragment() {
-    // TODO: Rename and change types of parameters
+
     private var param1: String? = null
     private var param2: String? = null
 
@@ -47,7 +46,7 @@ class ThirdFragment : Fragment() {
          * @param param2 Parameter 2.
          * @return A new instance of fragment ThirdFragment.
          */
-        // TODO: Rename and change types and number of parameters
+
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             ThirdFragment().apply {
